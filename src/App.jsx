ff84d7536d2296058c766a128d1fc59bbd95377f
Philemon Tebo Tebo <philballer41@gmail.com>
2024-01-20 12:06:39 +0100

    Integrating api routes with system storage in project directory

--- src/App.jsx
+++ src/App.jsx
@@ -22,30 +22,31 @@ function TodoHeader() {
 
 function TodoForm() {
   // Decleration of States
-  const [todo, setTodo] = useState([]);
   const [todoTask, setTodoTask] = useState('');
-  const [descript, setDescription] = useState('')
+  const [descript, setDescription] = useState('');
   const [taskData, setData] = useState([]);
-
 
   //Creating todo Task
   function CreateTodoTask(e) {
-    useEffect(() => {
-      e.preventDefault();
-      axios.post("http://127.0.0.1:3001/api/tasks", (name, description) => {
-        name: setTodoTask
-        description: setDescription
+    e.preventDefault();
+    const todoData = {
+      name: todoTask,
+      description: descript
+    }
+    axios.post("http://127.0.0.1:5001", todoData)
+      .then((res) => {
+        setData(res.data)
+        console.log(todoData)
+        console.log(res.data)
       })
-        .then((res) => {
-          setTodo(res.data)
-          console.log("Task Created successfully")
-        })
-    }, [todo])
+      .catch((err) => {
+        console.log(`Error connecting to server ${err}`)
+      })
   }
 
   //Getting data from the API's
   const getData = async () => {
-    const response = await fetch('http://127.0.0.1:3001/api/tasks')
+    const response = await fetch('http://127.0.0.1:5001/')
     try {
       const responseJson = await response.json();
       setData(responseJson);
@@ -61,34 +62,37 @@ function TodoForm() {
 
   //Delete task by passing user ID
   function deleteTodoTask(index) {
-    axios.delete(`http://127.0.0.1:3001/api/tasks/${index}`)
+    axios.delete(`http://127.0.0.1:5001/${index}`)
       .then((res) => {
-        setTodo(res.data)
-        console.log(`Task ${index} deleted successfully`);
+        setData(res.data)
+      })
+      .catch((err) => {
+        console.log(`Error: ${err}`)
       })
   }
 
   return (
     <div>
-      <form className="flex flex-col" onSubmit={(e) => CreateTodoTask(e)}>
+      <form className="flex flex-col" onSubmit={CreateTodoTask}>
         <label className='my-1 font-medium text-neutral-600'>Enter a task you have to do</label>
-        <input className="border-2 border-purple-400 rounded-md p-2 my-2" placeholder="Enter Name" name={todoTask} onChange={(event) => {
+        <input className="border-2 border-purple-400 rounded-md p-2 my-2" placeholder="Enter Name" value={todoTask} onChange={(event) => {
           setTodoTask(event.target.value)
         }} />
-        <input className="border-2 border-purple-400 rounded-md p-2 my-2" placeholder="Enter Description" name={descript} onChange={(event) => {
+        <input className="border-2 border-purple-400 rounded-md p-2 my-2" placeholder="Enter Description" value={descript} onChange={(event) => {
           setDescription(event.target.value)
         }} />
         <button className="bg-purple-800 rounded-md p-2 my-2 text-white hover:bg-purple-600 font-medium" type='submit'>Add</button>
       </form>
       <div className="grid grid-cols-3 gap-6 mt-4" >
+
         {/* Mapping taskData to list of tasks */}
         {
           taskData.map((item) => (
             <div className="flex flex-row justify-between" key={item.id}>
               <div className="flex flex-row md:flex-col">
                 <div className='flex flex-col'>
-                  <p className="ml-2 font-medium text-lg text-neutral-700 w-80">~ {item.name}</p>
-                  <p className="ml-2 font-medium text-lg text-neutral-700 w-80">~ {item.description}</p>
+                  <p className="ml-2 font-medium text-lg text-purple-800 w-80">~ {item.name}</p>
+                  <p className="ml-2 font-medium text-lg text-neutral-700 w-80">{item.description}</p>
                 </div>
               </div>
               <Trash className="text-red-400 hover:text-red-700 hover:fill-red-600" onClick={() => deleteTodoTask(item.id)} />
